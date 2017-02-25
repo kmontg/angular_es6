169b09639ce994b26c9605893aa6656db59e0ec9
Kyle Montgomery <kmontg003@gmail.com>
2017-02-24 22:49:55 -0500

    Finished ch.2

--- src/scope.js
+++ src/scope.js
@@ -7,6 +7,83 @@ export default class Scope {
     constructor(){
         this.$$watchers = [];
         this.$$lastDirtyWatch = null;
+        this.$$asyncQueue = [];
+        this.$$applyAsyncQueue = [];
+        this.$$applyAsyncId = null;
+        this.$$postDigestQueue = [];
+        this.$$phase = null;
+    }
+
+    // only executes, does not trigger digest
+    $eval(expr, locals) {
+        return expr(this, locals);
+    }
+
+    // attempts to $eval and then triggers $digest
+    $apply(expr) {
+        try {
+            this.$beginPhase('$apply');
+            this.$eval(expr);
+        } finally {
+            this.$clearPhase();
+            this.$digest();
+        }
+    }
+
+    // $eval during the current digest 
+    // triggers a digest if one isn't scheduled or one isn't ongoing when the timeout runs (i.e. the asyncQueue still has items)
+    $evalAsync(expr) {
+        if (!this.$$phase && !this.$$asyncQueue.length) {
+            setTimeout(() => {
+                if(this.$$asyncQueue.length) {
+                    this.$digest();
+                }
+            }, 0);
+        }
+        this.$$asyncQueue.push({
+                scope: this,
+                expression: expr
+            });
+    }
+
+    $applyAsync(expr) {
+        this.$$applyAsyncQueue.push(() => {
+            this.$eval(expr);
+        });
+        // coalesces many calls to $applyAsync
+        if(this.$$applyAsyncId === null) {
+            this.$$applyAsyncId = setTimeout(() => {
+                this.$apply(() => {
+                    this.$$flushApplyAsync();
+                });
+            }, 0);
+        }
+    }
+
+    $$flushApplyAsync() {
+        while (this.$$applyAsyncQueue.length) {
+            try {
+                this.$$applyAsyncQueue.shift()();
+            } catch(e) {
+                console.error(e);
+            } 
+        }
+        this.$$applyAsyncId = null;
+    }
+
+    $beginPhase(phase) {
+        if(this.$$phase) {
+            throw `${this.$$phase} already in progress.`;
+        }
+        this.$$phase = phase;
+    }
+
+    $clearPhase() {
+        this.$$phase = null;
+    }
+
+    $$postDigest(fn) {
+        this.$$postDigestQueue.push(fn);
     }
 
     $watch(watchFn, listenerFn, valueEq) {
@@ -31,13 +108,37 @@ export default class Scope {
         let dirty;
         let ttl = 10;
         this.$$lastDirtyWatch = null;
+        this.$beginPhase('$digest');
+
+        if (this.$$applyAsyncId) {
+            clearTimeout(this.$$applyAsyncId);
+            this.$$flushApplyAsync();
+        }
+
         do {
+            while (this.$$asyncQueue.length) {
+                try {
+                    let asyncTask = this.$$asyncQueue.shift();
+                    asyncTask.scope.$eval(asyncTask.expression);
+                } catch(e) {
+                    console.error(e);
+                }
+            }
             dirty = this.$$digestOnce();
-            ttl--; // http://stackoverflow.com/questions/971312/why-avoid-increment-and-decrement-operators-in-javascript
-            if (dirty && !ttl) {
+            if ((dirty || this.$$asyncQueue.length) && !(ttl--)) {
+                this.$clearPhase();
                 throw `${ttl} + 'digest iterations reached`;
             }
-        } while (dirty);
+        } while (dirty || this.$$asyncQueue.length);
+        this.$clearPhase();
+
+        while(this.$$postDigestQueue.length) {
+            try {
+                this.$$postDigestQueue.shift()();
+            } catch(e) {
+                console.error(e);
+            }
+        }
     }
 
     $$digestOnce() {
@@ -56,7 +157,7 @@ export default class Scope {
                             this);
                         dirty = true;
                     } else if (this.$$lastDirtyWatch === watcher) {
-                        // clean
+                        // clean short-circuit
                         return false;
                     }
                 }
@@ -75,6 +176,53 @@ export default class Scope {
             return newValue === oldValue ||
                 (typeof newValue === 'number' && typeof oldValue === 'number' &&
                     isNaN(newValue) && isNaN(oldValue));
+        }
+    }
+
+    $watchGroup(watchFns, listenerFn) {
+        // newValues/oldValues stored in closure
+        let newValues = new Array(watchFns.length);
+        let oldValues = new Array(watchFns.length);
+        let changeReactionScheduled = false;
+        let firstRun = true;
+
+        if (watchFns.length === 0) {
+            let shouldCall = true;
+            this.$evalAsync(() => {
+                if (shouldCall) {
+                    listenerFn(newValues, newValues, this);
+                }
+            });
+            return function() {
+                shouldCall = false;
+            };
+        }
+
+        function watchGroupListener() {
+            if (firstRun) {
+                firstRun = false;
+                listenerFn(newValues, newValues, this);
+            } else {
+                listenerFn(newValues, oldValues, this);
+            }
+            changeReactionScheduled = false;
+        }
+
+        let destroyFunctions = _.map(watchFns, (watchFn, i) => {
+            return this.$watch(watchFn, (newValue, oldValue) => {
+                newValues[i] = newValue;
+                oldValues[i] = oldValue;
+                if(!changeReactionScheduled) {
+                    changeReactionScheduled = true;
+                    this.$evalAsync(watchGroupListener);
+                }
+            });
+        });
+
+        return function() {
+            _.forEach(destroyFunctions, (destroyFunction) => {
+                destroyFunction();
+            });
         }
     }
 }
